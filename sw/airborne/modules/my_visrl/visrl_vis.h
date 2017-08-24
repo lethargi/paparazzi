9b3cb068acc9d091833aa5880c9c40c8ed45dabc
Manan Siddiquee <manan_siddiquee@hotmail.com>
2017-08-24 20:30:38 +0200

    removed 2 useless files

--- sw/airborne/modules/my_visrl/visrl_vis.h
+++
@@ -1,9 +0,0 @@
-#ifndef VISRL_VIS_H
-#define VISRL_VIS_H
-#include "modules/computer_vision/cv.h"
-
-// Module functions
-extern void visrl_vis_init(void);
-extern struct video_listener *listener;
-
-#endif /* VIS_AP_H */
